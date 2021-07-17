8e5680c9f03a508646cc50a68690f5fc4cf0dabe
U0I5708 <andrea.reguzzoni@esternibisp.it>
2021-07-17 18:19:55 +0200

    First Commit

---
+++ simpleChain.js
@@ -0,0 +1,53 @@
+/* ===== SHA256 with Crypto-js ===================================
+|  Learn more: Crypto-js: https://github.com/brix/crypto-js      |
+|  =============================================================*/
+
+const SHA256 = require('crypto-js/sha256');
+
+/* ===== Block Class ===================================
+|  Class with a constructor for block data model       |
+|  ====================================================*/
+
+class Block{
+	constructor(data){
+      
+      // Block data model
+      this.hash = "",
+      this.height = 0,
+      this.body = data,
+      this.time = 0,
+      this.previousblockhash = ""
+    }
+}
+
+
+/* ===== Blockchain ===================================
+|  Class with a constructor for blockchain data model  |
+|  with functions to support:                          |
+|     - createGenesisBlock()                           |
+|     - getLatestBlock()                               |
+|     - addBlock()                                     |
+|     - getBlock()                                     |
+|     - validateBlock()                                |
+|     - validateChain()                                |
+|  ====================================================*/
+
+class Blockchain{
+  constructor(){
+    this.chain = [];
+    this.addBlock(new Block("Genesis Block"))
+  }
+  
+  addBlock(newBlock){
+    // block height
+    newBlock.height = this.chain.length;
+    // UTC timestamp
+    newBlock.time = new Date().getTime().toString().slice(0,-3);
+    
+    if(this.chain.length > 0) {
+      newBlock.previousblockhash = this.chain[this.chain.length-1].hash;
+    }
+    newBlock.hash = SHA256(JSON.stringify(newBlock)).toString();
+  	this.chain.push(newBlock);
+  }
+}
